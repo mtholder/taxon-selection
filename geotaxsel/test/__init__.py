235e7c20030b70b294b2757e874eb49d21347d3e
Mark T. Holder <mtholder@gmail.com>
2023-10-02 15:30:47 -0500

    black code formatter

--- geotaxsel/test/__init__.py
+++ geotaxsel/test/__init__.py
@@ -18,13 +18,14 @@ import logging
 def get_test_file_names():
     """Get list of test file names."""
     path = os.path.dirname(__file__)
-    default_tests = _get_test_file_names_from_dir(path, 'geotaxsel.test.')
+    default_tests = _get_test_file_names_from_dir(path, "geotaxsel.test.")
     return default_tests
+
 
 def _get_test_file_names_from_dir(dirpath, pref):
     files = os.listdir(dirpath)
     t = []
-    pat = re.compile(r'^test.*\.py$')
+    pat = re.compile(r"^test.*\.py$")
     for f in files:
         if pat.match(f):
             rp = pref + f[:-3]  # [:-3] to strip ".py"
